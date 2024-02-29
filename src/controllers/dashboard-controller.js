5803cc95ddb44f74b22a0131c3ec52e122c85224
poraif <peadar.oraif@gmail.com>
2024-02-29 21:22:40 +0100

    feat: set up basic views

---
+++ src/controllers/dashboard-controller.js
@@ -0,0 +1,6 @@
+// import { PlaylistSpec } from "../models/joi-schemas.js";
+import { db } from "../models/db.js";
+
+export const dashboardController = {
+
+};
---
+++ src/controllers/placemark-controller.js
@@ -0,0 +1,6 @@
+// import { TrackSpec } from "../models/joi-schemas.js";
+import { db } from "../models/db.js";
+
+export const placemarkController = {
+ 
+};
